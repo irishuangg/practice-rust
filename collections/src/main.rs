2cf881c6127407dda6bae6714d13f6cd39411038
iris-engineering-data <30376255+iris-engineering-data@users.noreply.github.com>
2023-03-26 22:09:29 +0000

    lesson 8 more hashmaps

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     use std::collections::HashMap;
 
-    let &field_name = String::from("Favorite color");
-    let &field_value = String::from("Blue");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
 
-    let mut map = HashMap::new();
-    map.insert(&field_name, &field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
-    println!("{&field_name}")
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
 }
